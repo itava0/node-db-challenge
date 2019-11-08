70ca4adace3399df8b68c119d102d1a488acc10e
Ivan Tavarez <40846384+itava0@users.noreply.github.com>
2019-11-08 11:53:29 -0500

    Create a function to get single project based on the id

--- projects/projects-model.js
+++ projects/projects-model.js
@@ -2,9 +2,15 @@ const db = require('./data/db-config.js');
 
 module.exports = {
   get,
+  getById,
+  addProject
 }
 
 function get() {
   return db('project');
+}
+
+function getById() {
+  return db('project').where({id});
 }
 
