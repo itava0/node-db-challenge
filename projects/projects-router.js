364684d26f96b839b1a31bd9f6540049c445a7e6
Ivan Tavarez <40846384+itava0@users.noreply.github.com>
2019-11-08 12:49:21 -0500

    Create route to get a list of projects and individual one by giving id

--- projects/projects-router.js
+++ projects/projects-router.js
@@ -2,6 +2,31 @@ const express = require("express");
 const db = require("./projects-model.js");
 const router = express.Router();
 
+router.get("/", (req, res) => {
+  db.get()
+    .then(projects => {
+      res.status(200).json(projects);
+    })
+    .catch(err => {
+      res.status(500).json({ message: "Failed to get projects" });
+    });
+});
 
+router.get("/id", (req, res) => {
+  const { id } = req.param;
+  db.getById(id)
+    .then(project => {
+      if (project) {
+        res.status(200).json(project);
+      } else {
+        res
+          .status(404)
+          .json({ message: "Could not find project with given id." });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: "Failed to get project" });
+    });
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const helmet = require('helmet');
+const projectsRouter = require('./projects/projects-router.js')
 const server = express();
 
 const setting = [helmet(), express.json()]
 
 server.use(setting)
+server.use('/api/projects', projectsRouter);
 
 server.get('/', (req, res) => {
   res.send('Hello world')
